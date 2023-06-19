945ed062e12729f7c54a813f5cb55d6d3c4c3bba
jcoronelc <107409978+jcoronelc@users.noreply.github.com>
2023-06-19 13:16:01 -0500

    Add files via upload

---
+++ gestor-usuario.js
@@ -0,0 +1,3 @@
+// Código fuente del módulo Gestor del Usuario
+
+// Aquí iría el código fuente del módulo Gestor del Usuario
---
+++ gestor-usuario.test.js
@@ -0,0 +1,3 @@
+// Pruebas automatizadas para el módulo Gestor del Usuario
+
+// Aquí irían las pruebas automatizadas del módulo Gestor del Usuario
